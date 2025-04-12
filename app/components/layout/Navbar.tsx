f3dd14bfa5a0747898217522d2711c5bd309dab8
Jai Kar <41448157-karjai856@users.noreply.replit.com>
2025-04-12 20:31:17 +0000

    Restored to '2ab25fba66ce2a5d3a6551e6b6a04c9268ebb7bd'
    Replit-Restored-To: 2ab25fba66ce2a5d3a6551e6b6a04c9268ebb7bd

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -30,8 +30,8 @@ const Navbar = () => {
             <Image
               src="/logo.svg"
               alt="Materiiva Logo"
-              width={240}
-              height={80}
+              width={200}
+              height={60}
               priority
               className="dark:invert"
             />
